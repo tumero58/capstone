0fe33cc78d4fe1835ad171ce0428378411cf999b
tumero58 <erik.tumasyan@inomma.com>
2023-03-10 12:51:03 +0400

    feat: split warehouse page in half

--- app/pages/warehouse.tsx
+++ app/pages/warehouse.tsx
@@ -1,9 +1,11 @@
+import { styles } from "@/styles/Warehouse.styles";
 import { Box } from "@mui/material";
 
 export default function Warehouse(){
     return(
-        <Box>
-            Warehouse
+        <Box sx={styles.warehouseWrapper}>
+            <Box sx={styles.warehouseInfoBlock}></Box>
+            <Box sx={styles.warehouseViewBlock}></Box>
         </Box>
     )
 }
---
+++ app/styles/Warehouse.styles.ts
@@ -0,0 +1,16 @@
+export const styles = {
+    warehouseWrapper:{
+        display:"flex",
+        height:"100%"
+    },
+    warehouseInfoBlock:{
+        background:"red",
+        width:"100%",
+        height:"100%"
+    },
+    warehouseViewBlock:{
+        background:"green",
+        width:"100%",
+        height:"100%"
+    }
+};
